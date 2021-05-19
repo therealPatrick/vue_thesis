ca53608232dd7ee5ed4f4079ff2e4f70b64f44f3
Patrick <malamashangala13@gmail.com>
2021-05-19 16:29:14 +0800

    vue_thesis_grad

---
+++ components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>if you have any questions please dont hesistate to contact us</p>
+      </div>
+      <div class="column has-text-right">
+        <p>Developed by Patrick Malama Shangala 帕瑞克 | Made with ❤</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VmFooter"
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background: #b9e2fc;
+  color: black;
+}
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Company X
+        </h1>
+        <h2 class="subtitle">
+          Welcome to our Our Company here is the list of our products.
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  background: rgb(81, 186, 252);
+}
+h1,
+h2 {
+  color: #fff;
+}
+</style>
